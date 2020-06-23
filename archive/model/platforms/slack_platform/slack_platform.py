60c70ded085e438ac4eb0897881cdb5533a9385c
cluelessperson <5635253+cluelessperson@users.noreply.github.com>
2020-06-23 03:48:30 -0700

    3.7 shutil.copytree workaround :(

--- archive/model/platforms/slack_platform/slack_platform.py
+++ archive/model/platforms/slack_platform/slack_platform.py
@@ -2,7 +2,18 @@ from archive.model.platforms.platform import Platform
 from archive.model.platforms.slack_platform.slack_channel import SlackChannel
 from archive.common.graph.capabilities import Log
 from archive.model.platforms.slack_platform.slack_meta_client import SlackMetaClient
+import os
 import shutil
+
+
+def copytree(src, dst, symlinks=False, ignore=None):
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            shutil.copytree(s, d, symlinks, ignore)
+        else:
+            shutil.copy2(s, d)
 
 
 class SlackPlatform(Platform):
@@ -29,17 +40,17 @@ class SlackPlatform(Platform):
             self.add_children(SlackChannel(parent=self, name=channel, data=self.client.channels[channel]))
 
         # copy avatars
-        shutil.copytree(
+        #shutil.copytree(
+        copytree(
             self.raw_path() / '_avatars',
             self.html_path() / '_avatars',
-            # don't bother overwriting
             ignore=lambda path, files: [f for f in files if (self.html_path() / '_avatars' / f).is_file()],
         )
         # copy files
-        shutil.copytree(
+        #shutil.copytree(
+        copytree(
             self.raw_path() / '_files',
             self.html_path() / '_files',
-            # don't bother overwriting
             ignore=lambda path, files: [f for f in files if (self.html_path() / '_avatars' / f).is_file()],
         )
         self.html_template.render()
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='archive',
-    version='0.1.4',
+    version='0.1.5',
     url='',
     license='',
     author='clueless',
